2c26fc9c757fdd2e3c16cb628d275898b0704a3f
shamimBoot <74612674+shamimBoot@users.noreply.github.com>
2024-06-26 11:51:13 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,32 @@
+// create web server
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var path = require('path');
+var commentsPath = path.join(__dirname, 'comments.json');
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+
+// get comments
+app.get('/api/comments', function(req, res) {
+  fs.readFile(commentsPath, function(err, data) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    res.json(JSON.parse(data));
+  });
+});
+
+// add comments
+app.post('/api/comments', function(req, res) {
+  fs.readFile(commentsPath, function(err, data) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    var comments = JSON.parse(data);
+    var newComment = {
+      id: Date.now(),
